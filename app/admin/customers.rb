535a9771a1c882ccbe1e3f493264050b17dd559f
yingqiuli <yingqiuli@rrc.ca>
2024-04-05 14:37:45 -0500

    customer dashboard

---
+++ app/admin/customers.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Customer do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :name, :email, :address, :city, :province, :postal_code, :phone
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:name, :email, :address, :city, :province, :postal_code, :phone]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
