03f22ffc857ce8a05db96c6a7c24d2cc5675b822
yingqiuli <yingqiuli@rrc.ca>
2024-04-05 16:58:58 -0500

    tax

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,10 +6,25 @@ class OrdersController < ApplicationController
 
   private
 
-  def calculate_taxes(order)
-    province = order.customer.province
-    total =order.tax
-    {name:province,amount:total}
+  # def calculate_taxes(order)
+  #   province = order.customer.province
+  #   total =order.tax
+  #   {name:province,amount:total}
+  # end
+  def calculate_taxes(province, subtotal)
+    tax_info = TAX_RATES[province]
+
+    if tax_info.nil?
+      return { gst: 0, pst: 0, hst: 0, total_tax: 0 }
+    end
+
+    gst_amount = subtotal * tax_info[:gst]
+    pst_amount = subtotal * tax_info[:pst]
+    hst_amount = subtotal * tax_info[:hst]
+    total_tax = gst_amount + pst_amount + hst_amount
+
+    { gst: gst_amount, pst: pst_amount, hst: hst_amount, total_tax: total_tax }
   end
+
 end
 
