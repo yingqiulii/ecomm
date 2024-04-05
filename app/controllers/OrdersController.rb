b61dcdef49216dd9bbe5d530e32987553797f00e
yingqiuli <yingqiuli@rrc.ca>
2024-04-05 01:38:10 -0500

    check out5

--- app/controllers/OrdersController.rb
+++ app/controllers/OrdersController.rb
@@ -43,5 +43,6 @@
 class OrdersController < ApplicationController
   def show
     @order = Order.includes(:customer, :order_items).find(params[:id])
+    @taxes = calculate_taxes(@order)
   end
 end
