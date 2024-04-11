147a6b8bf91188657d9d9258ab56cadd236e50f5
yingqiuli <yingqiuli@rrc.ca>
2024-04-10 20:30:31 -0500

    checkoutscontroller done

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -25,31 +25,63 @@
 # end
 
 
+# class CheckoutsController < ApplicationController
+#   def confirm
+#     # 根据情况创建或找到顾客
+#     if customer_signed_in?
+#       customer = current_customer
+#     else
+#       name = params[:name]
+#       address = params[:address]
+#       province = params[:province]
+#       customer = Customer.find_or_create_by(name: name, address: address, province: province)
+#     end
+
+#     # 创建订单
+#     order = create_order(customer)
+
+#     # 根据创建结果重定向或渲染视图
+#     if order.persisted?
+#       redirect_to order_path(order), notice: "Order was successfully created."
+#     else
+#       render :new, status: :unprocessable_entity
+#     end
+#   end
+
+#   private
+
+#   def create_order(customer)
+#     order = Order.create(customer: customer)
+#     @cart.cart_items.each do |item|
+#       order.order_items.create(
+#         product: item.product,
+#         quantity: item.quantity,
+#         price: item.product.price
+#       )
+#     end
+#     order.save
+#     order
+#   end
+
+
+#   def customer_signed_in?
+#     !current_customer.nil?
+#   end
+# end
+
+
 class CheckoutsController < ApplicationController
-  # def confirm
-  #   # 根据情况创建或找到顾客
-  #   if customer_signed_in?
-  #     customer = current_customer
-  #   else
-  #     name = params[:name]
-  #     address = params[:address]
-  #     province = params[:province]
-  #     customer = Customer.find_or_create_by(name: name, address: address, province: province)
-  #   end
+  before_action :authenticate_customer!, only: [:confirm], unless: -> { request.format.js? }
 
-  #   # 创建订单
-  #   order = create_order(customer)
-
-  #   # 根据创建结果重定向或渲染视图
-  #   if order.persisted?
-  #     redirect_to order_path(order), notice: "Order was successfully created."
-  #   else
-  #     render :new, status: :unprocessable_entity
-  #   end
-  # end
   def confirm
-    # 使用表单提交的信息更新当前用户的信息，或者为未登录的用户创建新的Customer记录
+    # 创建或更新顾客信息
     customer = update_or_create_customer
+
+    # 如果无法保存顾客信息（比如，验证失败），则停止执行并返回
+    unless customer.persisted?
+      flash[:error] = "There was a problem with your information: " + customer.errors.full_messages.join(", ")
+      redirect_to new_order_path and return # 假设有一个用于显示表单的路径
+    end
 
     # 创建订单
     order = create_order(customer)
@@ -65,20 +97,19 @@ class CheckoutsController < ApplicationController
   private
 
   def update_or_create_customer
+    customer_attrs = { name: params[:name], address: params[:address], province: params[:province] }
     if customer_signed_in?
-      current_customer.update(customer_params)
+      current_customer.update(customer_attrs)
       current_customer
     else
-      Customer.create(customer_params)
+      Customer.find_or_create_by(customer_attrs)
     end
   end
 
-  private
-
   def create_order(customer)
-    order = Order.create(customer: customer)
+    order = Order.new(customer: customer)
     @cart.cart_items.each do |item|
-      order.order_items.create(
+      order.order_items.build(
         product: item.product,
         quantity: item.quantity,
         price: item.product.price
@@ -87,6 +118,7 @@ class CheckoutsController < ApplicationController
     order.save
     order
   end
+
 
   def customer_signed_in?
     !current_customer.nil?
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,35 +1,4 @@
-# class Order < ApplicationRecord
-#   belongs_to :customer
-#   has_many :order_items, dependent: :destroy
 
-#   def self.ransackable_associations(auth_object = nil)
-#     ["customer", "order_items"]
-#   end
-#   def self.ransackable_attributes(auth_object = nil)
-#     ["created_at", "customer_id", "id", "tax", "total", "updated_at"]
-#   end
-
-#   before_save :calculate_total_and_tax
-
-#   private
-
-#   def calculate_total_and_tax
-#     self.total = order_items.sum { |item| item.price * item.quantity }
-#     tax_rate = tax_rate_for_province
-#     self.tax = self.total * tax_rate
-#   end
-
-
-#   def tax_rate_for_province
-#     tax_rates = {
-#       'AB' => 0.05, 'BC' => 0.12, 'MB' => 0.12, 'NB' => 0.15,
-#   'NL' => 0.15, 'NT' => 0.05, 'NS' => 0.15, 'NU' => 0.05,
-#   'ON' => 0.13, 'PE' => 0.15, 'QC' => 0.14975, 'SK' => 0.11,
-#   'YT' => 0.05
-#     }
-#     tax_rates[customer.province] || 0.0
-#   end
-# end
 
 class Order < ApplicationRecord
   belongs_to :customer
